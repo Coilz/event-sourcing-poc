35e7e4428a68425c6f46a86ee015e06948070a44
Erwin Konink <erwinkonink@gmail.com>
2017-11-30 16:31:55 +0100

    Added an in memory storage

---
+++ src/EventSourcingPoc.Data/MongoDb/ReadModels/ShoppingCartItemReadModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EventSourcingPoc.Data.MongoDb.ReadModels
+{
+    public class ShoppingCartItemReadModel
+    {
+        public Guid ProductId { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
