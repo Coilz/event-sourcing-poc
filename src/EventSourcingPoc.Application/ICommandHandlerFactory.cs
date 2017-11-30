ed7975e1cb6a91f8436f41e48890d9eb70640ae8
Erwin Konink <erwinkonink@gmail.com>
2017-11-30 17:33:49 +0100

    Added an application layer

---
+++ src/EventSourcingPoc.Application/ICommandHandlerFactory.cs
@@ -0,0 +1,10 @@
+﻿using EventSourcingPoc.EventSourcing.Handlers;
+using EventSourcingPoc.Messages;
+
+namespace EventSourcingPoc.Application
+{
+    public interface ICommandHandlerFactory
+    {
+        ICommandHandler<TCommand> Resolve<TCommand>() where TCommand : ICommand;
+    }
+}
---
+++ src/EventSourcingPoc.Application/IDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using EventSourcingPoc.Messages;
+
+namespace EventSourcingPoc.Application
+{
+    public interface IDispatcher<in TMessage> where TMessage : ICommand
+    {
+        void Send(TMessage command);
+    }
+}
