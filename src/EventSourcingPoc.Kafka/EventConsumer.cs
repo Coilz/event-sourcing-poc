f7453d8a3a0394f3cb81443c6b05024202e493e0
Erwin Konink <erwinkonink@gmail.com>
2017-12-10 12:47:37 +0000

    Added a kafka consumer and producer - in progress

---
+++ src/EventSourcingPoc.Kafka/EventConsumer.cs
@@ -0,0 +1,154 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using EventSourcingPoc.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcingPoc.Kafka
+{
+    public class EventConsumer : IDisposable
+    {
+        private const int Timeout = 100;
+        private Consumer<string, Event> _consumer;
+        private string _brokerList;
+        private List<string> _topics;
+
+        public EventConsumer()
+        {
+            _topics = new List<string> { "firstTopic", "secondTopic" };
+
+            var config = ConstructConfig(_brokerList, false);
+            _consumer = new Consumer<string, Event>(config, new StringDeserializer(Encoding.UTF8), null); // TODO: Create an Event serializer
+            _consumer.OnConsumeError += Consumer_OnConsumeError;
+            _consumer.OnError += Consumer_OnError;
+            _consumer.OnLog += Consumer_OnLog;
+            _consumer.OnMessage += Consumer_OnMessage;
+            _consumer.OnOffsetsCommitted += Consumer_OnOffsetsCommitted;
+            _consumer.OnPartitionEOF += Consumer_OnPartitionEOF;
+            _consumer.OnPartitionsAssigned += Consumer_OnPartitionsAssigned;
+            _consumer.OnPartitionsRevoked += Consumer_OnPartitionsRevoked;
+            _consumer.OnStatistics += Consumer_OnStatistics;
+        }
+
+        public bool Consuming { get; private set; }
+
+        public void Start()
+        {
+            if (Consuming) return;
+
+            Consuming = true;
+            _consumer.Subscribe(_topics);
+
+            while (Consuming)
+            {
+                if (!_consumer.Consume(out Message<string, Event> message, TimeSpan.FromMilliseconds(Timeout)))
+                {
+                    continue;
+                }
+
+                if (message.Offset % 5 == 0)
+                {
+                    var committedOffsets = _consumer.CommitAsync(message).Result;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            if (!Consuming) return;
+
+            Consuming = false;
+            _consumer.Unsubscribe();
+
+            Task.Delay(TimeSpan.FromMilliseconds(Timeout)).GetAwaiter().GetResult();
+        }
+
+        private void Consumer_OnStatistics(object sender, string e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Consumer_OnPartitionsRevoked(object sender, List<TopicPartition> e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Consumer_OnPartitionsAssigned(object sender, List<TopicPartition> e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Consumer_OnPartitionEOF(object sender, TopicPartitionOffset e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Consumer_OnOffsetsCommitted(object sender, CommittedOffsets e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Consumer_OnMessage(object sender, Message<string, Event> e)
+        {
+            // TODO: handle the message
+        }
+
+        private void Consumer_OnLog(object sender, LogMessage e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Consumer_OnError(object sender, Error e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Consumer_OnConsumeError(object sender, Message e)
+        {
+            // TODO: log stuff here
+        }
+
+        private static Dictionary<string, object> ConstructConfig(string brokerList, bool enableAutoCommit) =>
+            new Dictionary<string, object>
+            {
+                { "group.id", "advanced-csharp-consumer" },
+                { "enable.auto.commit", enableAutoCommit },
+                { "auto.commit.interval.ms", 5000 },
+                { "statistics.interval.ms", 60000 },
+                { "bootstrap.servers", brokerList },
+                { "default.topic.config", new Dictionary<string, object>()
+                    {
+                        { "auto.offset.reset", "smallest" }
+                    }
+                }
+            };
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Stop();
+                    _consumer.Dispose();
+                    _consumer = null;
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ src/EventSourcingPoc.Kafka/EventProducer.cs
@@ -0,0 +1,78 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using EventSourcingPoc.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcingPoc.Kafka
+{
+    public class EventProducer : IDisposable
+    {
+        private Producer<string, Event> _producer;
+        private string _topicName;
+
+        public EventProducer()
+        {
+            string brokerList = "the first broker;the next broker";
+            _topicName = "my special topic";
+
+            var config = new Dictionary<string, object> { { "bootstrap.servers", brokerList } };
+
+            _producer = new Producer<string, Event>(config, new StringSerializer(Encoding.UTF8), null); // TODO: create an EventSerializer
+            _producer.OnError += Producer_OnError;
+            _producer.OnStatistics += Producer_OnStatistics;
+            _producer.OnLog += Producer_OnLog;
+        }
+
+        public async Task Produce(Event @event)
+        {
+            var deliveryReport = await _producer.ProduceAsync(_topicName, null, @event);
+            // TODO: log result stuff here
+        }
+
+        private void Producer_OnError(object sender, Error e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Producer_OnLog(object sender, LogMessage e)
+        {
+            // TODO: log stuff here
+        }
+
+        private void Producer_OnStatistics(object sender, string e)
+        {
+            // TODO: log stuff here
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Tasks are not waited on synchronously (ContinueWith is not synchronous),
+                    // so it's possible they may still in progress here.
+                    _producer.Flush(TimeSpan.FromSeconds(10));
+                    _producer.Dispose();
+                    _producer = null;
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+    }
+}
